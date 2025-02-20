44829283228107d0497f880798679fb24a84ef10
MarieNICOLAS <marie.nicolas29@hotmail.com>
2025-02-20 21:36:44 +0100

    feat(header): Add vertical navbar

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("../components/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("afterbegin", data);
+        });
+
+    fetch("../components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("beforeend", data);
+        });
+});
