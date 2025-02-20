5a37b00b680d975fd3f03b66dece1f92d3c2eea7
MarieNICOLAS <marie.nicolas29@hotmail.com>
2025-02-20 21:45:12 +0100

    feat(js): Set dynamic page title

--- js/script.js
+++ js/script.js
@@ -11,3 +11,23 @@ document.addEventListener("DOMContentLoaded", function() {
             document.body.insertAdjacentHTML("beforeend", data);
         });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Définition des titres par page
+    const pageTitles = {
+        "index.html": "Accueil | Portfolio Marie Nicolas",
+        "projects.html": "Projets | Portfolio Marie Nicolas",
+        "experiences.html": "Expériences | Portfolio Marie Nicolas",
+        "about.html": "À Propos | Portfolio Marie Nicolas"
+    };
+
+    // Récupérer le nom du fichier actuel
+    let page = window.location.pathname.split("/").pop();
+
+    // Vérifier si le fichier correspond à une entrée dans l'objet pageTitles
+    if (pageTitles[page]) {
+        document.title = pageTitles[page]; // Modifier le titre
+    } else {
+        document.title = "Portfolio Marie Nicolas"; // Titre par défaut
+    }
+});
